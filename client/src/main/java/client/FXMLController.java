6f1d41d4bee9c23b85d2a91f00a8f6788212d51e
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2020-11-18 23:03:32 +0900

    tried to make TextField

--- client/src/main/java/client/FXMLController.java
+++ client/src/main/java/client/FXMLController.java
@@ -5,9 +5,17 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
+
+  @FXML
+  private TextField testTextField;
+
+  @FXML
+  public Button btn0;
 
   @FXML
   private Label label;
@@ -17,5 +25,13 @@ public class FXMLController implements Initializable {
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version");
     label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+  }
+
+  public void buttonClicked(ActionEvent actionEvent) {
+    Button btnClicked = (Button) actionEvent.getSource();
+    String id = btnClicked.getId();
+    if (id.equals("btn0")){
+      System.out.println("First Button!");
+    }
   }
 }
--- client/src/main/java/client/MainApp.java
+++ client/src/main/java/client/MainApp.java
@@ -2,20 +2,42 @@ package client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
+  TextField tf;
+  VBox vbox;
+  Scene scene;
 
   @Override
   public void start(Stage stage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
 
-    Scene scene = new Scene(root);
+    // text field
+    tf = new TextField();
+    vbox = new VBox();
+    vbox.getChildren().add(tf);
+
+    stage.setTitle("Couch Sloth");
+    Scene scene = new Scene(vbox, 1000, 800);
+//    Scene scene = new Scene(root, 1000, 800);
     scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
-    stage.setTitle("JavaFX and Gradle");
+//    GridPane gridPane = (GridPane) scene.getRoot();
+//    TextField message = new TextField();
+//    message.setTranslateX(115);
+//    message.setTranslateY(-70);
+//    gridPane.getChildren().add(message);
+
+
+//    scene = new Scene(vbox);
+
     stage.setScene(scene);
     stage.show();
   }
