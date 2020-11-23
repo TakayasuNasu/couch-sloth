6c6507059ac472053a82c85a8243b8e85d67c0f7
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2020-11-24 08:25:25 +0900

    replaced image with youtube

--- client-react/src/components/Player.js
+++ client-react/src/components/Player.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import bgMovie from '../img/bg-movie.png'
+import ReactPlayer from 'react-player'
 
 const Div = styled.div`
   width: 912px;
@@ -11,7 +12,8 @@ const Div = styled.div`
 `
 
 const Player = props => (
-  <Div><img src={bgMovie} alt="back ground image."/></Div>
+  // <Div><img src={bgMovie} alt="back ground image."/></Div>
+  <Div><ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' width='100%' height='100%'/></Div>
 )
 
 export default Player
